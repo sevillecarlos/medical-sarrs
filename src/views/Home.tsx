bd4d31486056e26cd4d59e987739eea01334f447
sevillecarlos <sevillecarlos@gmail.com>
2021-08-04 14:38:54 -0600

    add packages

---
+++ src/views/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hola</p>
+    </div>
+  );
+};
+
+Home.propTypes = {};
+
+export default Home;
